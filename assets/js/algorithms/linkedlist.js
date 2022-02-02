a9f5528541ddab008ef6d00fd829132aa35ab0ea
haruperi <r.haruperi@hotmail.com>
2022-02-02 18:57:12 +0200

    Datastructures Linked List - Shift

--- assets/js/algorithms/linkedlist.js
+++ assets/js/algorithms/linkedlist.js
@@ -59,6 +59,22 @@ class Linkedlist {
         return this
 
     }
+
+    shiftNode(){
+        if(!this.head)
+            return undefined
+
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+
+        if (this.length===0) {
+            this.tail = null
+        }
+
+        return temp
+    }
 }
 
 
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,16 @@ import {Linkedlist} from "./algorithms/linkedlist.js";
 
 let myLinkedList = new Linkedlist(11)
 myLinkedList.pushNewNode(3)
-myLinkedList.pushNewNode(23)
-myLinkedList.pushNewNode(7)
+//myLinkedList.pushNewNode(23)
+//myLinkedList.pushNewNode(7)
+
 //console.log(myLinkedList.popNode())
 //console.log(myLinkedList.popNode())
 //console.log(myLinkedList.popNode())
-myLinkedList.unshiftNode(4)
+
+//myLinkedList.unshiftNode(4)
+
+console.log(myLinkedList.shiftNode())
+console.log(myLinkedList.shiftNode())
+console.log(myLinkedList.shiftNode())
 console.log(myLinkedList)
