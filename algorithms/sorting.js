99c06ed09548da024c02ce639d474400919b6d1d
Haruperi <r.haruperi@hotmail.com>
2022-03-29 08:39:08 +0200

    Selection Sort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -13,7 +13,24 @@ const bubbleSort = (arr) => {
     return arr
 }
 
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j]<arr[min]) min = j
+        }
+        if (i !== min) {
+            let temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        }
+    }
+    return arr
+}
+
+
 
 
 
 exports.bubbleSort = bubbleSort
+exports.selectionSort = selectionSort
