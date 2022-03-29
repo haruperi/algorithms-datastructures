2954b413a6476b4007b1b611bc69a596914178bf
Haruperi <r.haruperi@hotmail.com>
2022-03-29 08:37:30 +0200

    Bubble Sort

---
+++ algorithms/sorting.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const bubbleSort = (arr) => {
+    for (let i = arr.length-1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j+1]){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+
+
+
+exports.bubbleSort = bubbleSort
