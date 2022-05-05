1ea1dfc7803853b8a3c94819cbb7e1f2f0d4f7a1
haruperi <r.haruperi@hotmail.com>
2022-05-05 13:34:39 +0200

    Added Swap Method

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -1,49 +1,5 @@
 'use strict'
 
-const bubbleSort = (arr) => {
-    for (let i = arr.length-1; i > 0; i--) {
-        for (let j = 0; j < i; j++) {
-            if (arr[j] > arr[j+1]){
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-            }
-        }
-    }
-    return arr
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
 }
-
-const selectionSort = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        let min = i
-        for (let j = i; j < arr.length; j++) {
-            if (arr[j]<arr[min]) min = j
-        }
-        if (i !== min) {
-            let temp = arr[i]
-            arr[i] = arr[min]
-            arr[min] = temp
-        }
-    }
-    return arr
-}
-
-const insertionSort = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        let temp = arr[i]
-        let j = i+1
-        while (j>-1){
-            if (arr[j]<temp){
-                arr[i] = arr[j]
-            }
-            j--
-        }
-    }
-
-    return arr
-}
-
-
-
-exports.bubbleSort = bubbleSort
-exports.selectionSort = selectionSort
