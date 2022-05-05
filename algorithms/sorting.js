58a93c03fb2d86c7ba9e7293f5650c13d52a59ec
haruperi <r.haruperi@hotmail.com>
2022-05-05 13:36:03 +0200

    Added selectionSort Method

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -18,3 +18,17 @@ const bubbleSort = (arr) => {
     }
     return arr
 }
+
+// O(N^2)
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[min])
+                min = j
+        }
+        if (min !== i)
+            swap(arr,i,min)
+    }
+    return arr
+}
